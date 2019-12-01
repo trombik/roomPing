e512c43924da0f175b673b09787c2d184bf96ef3
Tomoyuki Sakurai <y@trombik.org>
2019-12-01 12:30:50 +0900

    bugfix: fix accidentally formated files as C file

--- src/main/component.mk
+++ src/main/component.mk
@@ -1,2 +1,2 @@
 COMPONENT_ADD_INCLUDEDIRS = .
-                            COMPONENT_OBJS : =  main.o wifi_connect.o sntp_connect.o task_icmp_client.o task_publish.o
+COMPONENT_OBJS :=  main.o wifi_connect.o sntp_connect.o task_icmp_client.o task_publish.o
