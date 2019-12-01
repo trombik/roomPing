9936649423fe84a64afe9fea218388d0ef734bab
Tomoyuki Sakurai <y@trombik.org>
2019-12-01 18:30:52 +0900

    bugfix: remove unsued variable

--- src/main/wifi_connect.c
+++ src/main/wifi_connect.c
@@ -28,7 +28,6 @@ static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
         s_retry_num = 0;
-        ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
         ESP_LOGI(TAG, "Connected to the AP");
     }
 }
