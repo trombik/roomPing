0ced50267235a23781a1b3d65920cb89ae08ed97
Tomoyuki Sakurai <y@trombik.org>
2019-12-02 13:49:14 +0900

    bugfix: add missing certs/ca_cert_ota.pem

--- src/main/component.mk
+++ src/main/component.mk
@@ -5,4 +5,5 @@ COMPONENT_OBJS :=  main.o \
 	task_icmp_client.o \
 	task_publish.o \
 	task_ota.o
-COMPONENT_EMBED_TXTFILES := cert.pem
+COMPONENT_EMBED_TXTFILES := cert.pem \
+	certs/ca_cert_ota.pem
