11327d65438d2f592ef719c6a2cfdbd4393f9d66
Tomoyuki Sakurai <y@trombik.org>
2019-12-02 14:33:43 +0900

    doc: add LICENSE
    fixes #9

--- src/main/wifi_connect.h
+++ src/main/wifi_connect.h
@@ -1,3 +1,11 @@
+/*
+   This code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
 #include <esp_err.h>
 
 esp_err_t init_wifi(void);
