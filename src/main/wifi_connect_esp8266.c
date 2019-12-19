dd400ffbd5251a9dd56aa6fe36342a4f9a0c4c45
Tomoyuki Sakurai <y@trombik.org>
2019-12-19 16:51:22 +0900

    bugfix: fix wrong variable names

--- src/main/wifi_connect_esp8266.c
+++ src/main/wifi_connect_esp8266.c
@@ -14,6 +14,7 @@
 #include <esp_err.h>
 #include <esp_log.h>
 #include <mqtt_client.h>
+#include <esp_event_loop.h>
 
 #include "wifi_connect.h"
 
@@ -33,7 +34,7 @@ esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
             esp_wifi_connect();
             break;
         case SYSTEM_EVENT_STA_GOT_IP:
-            xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+            xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
 
             break;
         case SYSTEM_EVENT_STA_DISCONNECTED:
@@ -45,7 +46,7 @@ esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
             }
             if (s_retry_num < MAXIMUM_RETRY) {
                 esp_wifi_connect();
-                xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+                xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
                 s_retry_num++;
                 ESP_LOGI(TAG, "Retrying to connect to the AP");
             }
@@ -59,22 +60,22 @@ esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
 esp_err_t init_wifi()
 {
     tcpip_adapter_init();
-    wifi_event_group = xEventGroupCreate();
+    s_wifi_event_group = xEventGroupCreate();
     ESP_ERROR_CHECK(esp_event_loop_init(wifi_event_handler, NULL));
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
     wifi_config_t wifi_config = {
         .sta = {
-            .ssid = CONFIG_WIFI_SSID,
-            .password = CONFIG_WIFI_PASSWORD,
+            .ssid = CONFIG_PROJECT_WIFI_SSID,
+            .password = CONFIG_PROJECT_WIFI_PASSWORD,
         },
     };
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-    ESP_LOGI(TAG, "start the WIFI SSID:[%s]", CONFIG_WIFI_SSID);
+    ESP_LOGI(TAG, "start the WIFI SSID:[%s]", CONFIG_PROJECT_WIFI_SSID);
     ESP_ERROR_CHECK(esp_wifi_start());
     ESP_LOGI(TAG, "Waiting for wifi");
-    xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
+    xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT, false, true, portMAX_DELAY);
     return ESP_OK;
 }
