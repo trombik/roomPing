61ebbfea2eedcd874dc16c8843248e031b98dfb4
Tomoyuki Sakurai <y@trombik.org>
2019-12-19 17:49:59 +0900

    bugfix: adjust stack sizes
    with this configuration, ESP32 does not crash

--- src/main/constant_task.h
+++ src/main/constant_task.h
@@ -1,14 +1,16 @@
 #if !defined(__CONSTANT_TASK__H__)
 #define __CONSTANT_TASK__H__
 
+/* current measured freeheap 184244 */
+
 #define ICMP_TASK_STACK_SIZE (configMINIMAL_STACK_SIZE * 3)
 #define ICMP_TASK_PRIORITY (10)
 
-#define PUBLISH_TASK_STACK_SIZE (configMINIMAL_STACK_SIZE * 20)
+#define PUBLISH_TASK_STACK_SIZE (configMINIMAL_STACK_SIZE * 5)
 #define PUBLISH_TASK_PRIORITY (5)
 
-#define MQTT_TASK_STACK_SIZE (configMINIMAL_STACK_SIZE * 11)
+#define MQTT_TASK_STACK_SIZE (configMINIMAL_STACK_SIZE * 3)
 
-#define ESP_PING_STACK_SIZE (configMINIMAL_STACK_SIZE * 11)
+#define ESP_PING_STACK_SIZE (configMINIMAL_STACK_SIZE * 3)
 
 #endif
