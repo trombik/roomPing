d537660ce2444f2d1cb97b2f20563b80a1a4adbf
Tomoyuki Sakurai <y@trombik.org>
2019-12-19 18:34:48 +0900

    bugfix: record current freeheap on ESP32 and ESP8266

--- src/main/constant_task.h
+++ src/main/constant_task.h
@@ -1,7 +1,14 @@
 #if !defined(__CONSTANT_TASK__H__)
 #define __CONSTANT_TASK__H__
 
-/* current measured freeheap 184244 */
+/* current measured freeheap
+ * ESP32 184244
+ * ESP8266 44536 (without icmp_client)
+ *
+ * configMINIMAL_STACK_SIZE
+ * ESP32 2048
+ * ESP8266 768
+ * */
 
 #define ICMP_TASK_STACK_SIZE (configMINIMAL_STACK_SIZE * 3)
 #define ICMP_TASK_PRIORITY (10)
